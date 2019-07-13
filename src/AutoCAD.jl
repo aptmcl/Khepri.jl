b4dea279f6ed1a2c678770df22d1a0e27ae6dbdb
aptmcl <antonio.menezes.leitao@gmail.com>
2019-07-13 20:08:20 +0100

    Fixed spline introspection.

--- src/AutoCAD.jl
+++ src/AutoCAD.jl
@@ -944,7 +944,7 @@ shape_from_ref(r, b::ACAD=current_backend()) =
                  backend=b, ref=ref)
         elseif code == 7
             let tans = ACADSplineTangents(c, r)
-                if length(tans) == 0 || (length(tans[1]) < 1e-20 && length(tans[2]) < 1e-20)
+                if length(tans[1]) < 1e-20 && length(tans[2]) < 1e-20
                     closed_spline(ACADSplineInterpPoints(c, r)[1:end-1],
                                   backend=b, ref=ref)
                 else
@@ -987,6 +987,9 @@ shape_from_ref(r, b::ACAD=current_backend()) =
         elseif 103 <= code <= 106
             polygon(ACADLineVertices(c, r),
                     backend=b, ref=ref)
+        elseif code == 107
+            closed_spline(ACADSplineInterpPoints(c, r)[1:end-1],
+                          backend=b, ref=ref)
         else
             #unknown(backend=b, ref=ref)
             unknown(r, backend=b, ref=LazyRef(b, ACADNativeRef(r), 0, 0))# To force copy
