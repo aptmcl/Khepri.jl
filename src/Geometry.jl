657aabec4657ccaf7c8c15259ed16f65c1334a96
aptmcl <antonio.menezes.leitao@gmail.com>
2019-09-07 17:23:55 +0100

    Fixed indentation. Added offset specializations for paths. Added subtract operation for polygon vertices.

--- src/Geometry.jl
+++ src/Geometry.jl
@@ -30,26 +30,85 @@ polygonal line that is parallel to the first one.
 =#
 
 v_in_v(v0, v1) =
-    let v = v0 + v1
-        v*dot(v0, v0)/dot(v, v0)
-    end
+  let v = v0 + v1
+    v*dot(v0, v0)/dot(v, v0)
+  end
 
 rotated_v(v, alpha) =
-    vpol(pol_rho(v), pol_phi(v) + alpha)
+  vpol(pol_rho(v), pol_phi(v) + alpha)
 
 centered_rectangle(p0, w, p1) =
-    let v0 = p1 - p0
-        v1 = rotated_v(v0, pi/2)
-        c = loc_from_o_vx_vy(p0, v0, v1)
-        rectangle(c-vy(w/2, c.cs), distance(p0, p1), w)
-    end
+  let v0 = p1 - p0,
+      v1 = rotated_v(v0, pi/2),
+      c = loc_from_o_vx_vy(p0, v0, v1)
+    rectangle(c-vy(w/2, c.cs), distance(p0, p1), w)
+  end
 
 offset(ps::Locs, d::Real) =
-    let vs = map((p0, p1) -> rotated_v(unitized(p1 - p0)*d, pi/2), ps[2:end], ps[1:end-1])
-        vs = [vs[1], map(v_in_v, vs[1:end-1], vs[2:end])..., vs[end]]
-        map(+, ps, vs)
-    end
+  let vs = map((p0, p1) -> rotated_v(unitized(p1 - p0)*d, pi/2), ps[2:end], ps[1:end-1]),
+      vs = [vs[1], map(v_in_v, vs[1:end-1], vs[2:end])..., vs[end]]
+    map(+, ps, vs)
+  end
 
+offset(path::OpenPolygonalPath, d::Real) = open_polygonal_path(offset(path.vertices, d))
+offset(path::ClosedPolygonalPath, d::Real) = closed_polygonal_path(offset(path.vertices, d))
 offset(l::Line, d::Real) = line(offset(l.vertices, d))
 
 export offset
+
+# Polygon combination
+
+closest_vertices_indexes(pts1, pts2) =
+  # This is a brute force method. There are better algorithms to do this.
+  let min_dist = Inf,
+      min_i = nothing,
+      min_j = nothing
+    for (i, pt1) in enumerate(pts1)
+      for (j, pt2) in enumerate(pts2)
+        let dist = distance(pt1, pt2)
+          if dist < min_dist
+            min_dist = dist
+            min_i = i
+            min_j = j
+          end
+        end
+      end
+    end
+    min_i, min_j
+  end
+
+#=
+using Test
+pts1 = regular_polygon_vertices(4, xy(1,2), 4)
+pts2 = regular_polygon_vertices(4, xy(2,2), 1)
+pts3 = regular_polygon_vertices(5, xy(1,4), 1)
+pts4 = regular_polygon_vertices(6, xy(0,1), 1)
+
+polygon(pts1)
+polygon(pts2)
+polygon(pts3)
+polygon(pts4)
+
+@test closest_vertices_indexes(pts1, pts2) == (1,1)
+@test closest_vertices_indexes(pts1, pts3) == (2,2)
+@test closest_vertices_indexes(pts1, pts4) == (4,6)
+@test closest_vertices_indexes(pts2, pts3) == (2,5)
+=#
+
+subtract_polygon_vertices(pts1, pts2) =
+  let (i, j) = closest_vertices_indexes(pts1, pts2)
+    [pts1[1:i]..., reverse([pts2[j:end]..., pts2[1:j]...])..., pts1[i:end]...]
+  end
+
+export subtract_polygon_vertices
+
+#=
+pts1 = subtract_polygon_vertices(pts1, pts2)
+pts1 = subtract_polygon_vertices(pts1, pts3)
+pts1 = subtract_polygon_vertices(pts1, pts4)
+polygon(pts1)
+for (i,p) in enumerate(pts1)
+  text(string(i), p, 0.1)
+  sleep(1)
+end
+=#
