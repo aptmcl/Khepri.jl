9b7d1fdaaa590d91438bcc20f1a776e99cff4d3f
aptmcl <antonio.menezes.leitao@gmail.com>
2019-09-09 23:16:57 +0100

    Improved implementation of walls.

--- src/Radiance.jl
+++ src/Radiance.jl
@@ -25,7 +25,7 @@ write_rad_primitive(io::IO, modifier, typ, identifier, strings, ints, reals) =
 
 write_rad_polygon(io::IO, modifier, id, vertices) =
   begin
-    with(current_backend, autocad, immediate_mode, true) do
+    with(current_backend, autocad) do
       polygon(vertices)
     end
     println(io, modifier, " ", "polygon", " ", id)
@@ -144,6 +144,7 @@ next_modifier(b::Radiance, s::Shape) =
 
 save_rad(path::String, b::RadianceBackend=radiance) =
   let buf = b.buffer()
+    take!(buf)
     for s in b.shapes
       realize(b, s)
     end
@@ -157,7 +158,7 @@ save_rad(path::String, b::RadianceBackend=radiance) =
 current_backend(radiance)
 
 delete_all_shapes(b::Radiance) =
-  take!(radiance.buffer())
+  (empty!(b.shapes); nothing)
 
 
 
@@ -255,17 +256,29 @@ realize(b::Radiance, s::Rotate) =
 
 # BIM
 
-realize_pyramid_fustrum(b::Radiance, s::Shape, kind::String, bot_vs, top_vs) =
-  let bot_id = next_id(b, s),
-      top_id = next_id(b, s),
-      mod = next_modifier(b, s),
+realize_pyramid_fustrum(b::Radiance, s::Shape, kind::String, bot_path::Path, top_path::Path, closed=true) =
+  realize_pyramid_fustrum(b, s, kind, path_vertices(bot_path), path_vertices(top_path), closed)
+
+realize_pyramid_fustrum(b::Radiance, s::Shape, kind::String, bot_vs, top_vs, closed=true) =
+  let mod = next_modifier(b, s),
       buf = buffer(b)
-    write_rad_polygon(buf, "mat$(kind)top$(mod)", bot_id, reverse(bot_vs))
-    write_rad_polygon(buf, "mat$(kind)bot$(mod)", top_id, top_vs)
+    if closed
+      write_rad_polygon(buf, "mat$(kind)top$(mod)", next_id(b, s), reverse(bot_vs))
+      write_rad_polygon(buf, "mat$(kind)bot$(mod)", next_id(b, s), top_vs)
+    end
     for vs in zip(bot_vs, circshift(bot_vs, 1), circshift(top_vs, 1), top_vs)
       write_rad_polygon(buffer(b), "mat$(kind)side$(mod)", next_id(b, s), vs)
     end
-    bot_id
+  end
+
+realize_polygon(b::Radiance, s::Shape, kind::String, path::Path, acw=true) =
+  realize_polygon(b, s, kind, path_vertices(path), acw)
+
+realize_polygon(b::Radiance, s::Shape, kind::String, vs, acw=true) =
+  let mod = next_modifier(b, s),
+      buf = buffer(b),
+      side = acw ? "top" : "bot"
+    write_rad_polygon(buf, "mat$(kind)$(side)$(mod)", next_id(b, s), acw ? vs : reverse(vs))
   end
 
 realize(b::Radiance, s::Slab) =
@@ -310,31 +323,63 @@ This forces us to create multiple subpaths.
 realize(b::Radiance, w::Wall) =
   let w_base_height = w.bottom_level.height,
       w_height = w.top_level.height - w_base_height,
-      w_path = translate(w.path, vz(w_base_height)),
       r_thickness = r_thickness(w),
       l_thickness = l_thickness(w),
+      w_path = translate(w.path, vz(w_base_height)),
+      w_paths = subpaths(w_path),
       r_w_paths = subpaths(offset(w_path, r_thickness)),
       l_w_paths = subpaths(offset(w_path, l_thickness)),
-      openings = [w.doors..., w.windows...]
-    for (r_w_path, l_w_path) in zip(r_w_paths, l_w_paths)
-      let r_w_path = closed_path_for_height(r_w_path, w_height),
-          l_w_path = closed_path_for_height(l_w_path, w_height)
-        #@assert length(path_vertices(w_path)) == 2 # fix this for bigger numbers
-        for op in openings
-          let op_base_height = op.loc.y,
-              op_height = op.family.height,
-              op_path = translate(subpath(w_path, op.loc.x, op.loc.x + op.family.width), vz(op_base_height)),
-              r_op_path = closed_path_for_height(offset(op_path, r_thickness), op_height),
-              l_op_path = closed_path_for_height(offset(op_path, l_thickness), op_height)
-            r_w_path = subtract_paths(r_w_path, r_op_path)
-            l_w_path = subtract_paths(l_w_path, l_op_path)
+      openings = [w.doors..., w.windows...],
+      prevlength = 0
+    for (w_seg_path, r_w_path, l_w_path) in zip(w_paths, r_w_paths, l_w_paths)
+      let currlength = prevlength + path_length(w_seg_path),
+          c_r_w_path = closed_path_for_height(r_w_path, w_height),
+          c_l_w_path = closed_path_for_height(l_w_path, w_height)
+        realize_pyramid_fustrum(b, w, "wall", c_l_w_path, c_r_w_path, false)
+        openings = filter(openings) do op
+          if prevlength <= op.loc.x < currlength ||
+             prevlength <= op.loc.x + op.family.width <= currlength # contained (at least, partially)
+            let op_height = op.family.height,
+                op_at_start = op.loc.x <= prevlength,
+                op_at_end = op.loc.x + op.family.width >= currlength,
+                op_path = subpath(w_path,
+                                  max(prevlength, op.loc.x),
+                                  min(currlength, op.loc.x + op.family.width)),
+                r_op_path = offset(op_path, r_thickness),
+                l_op_path = offset(op_path, l_thickness),
+                fixed_r_op_path =
+                  open_polygonal_path([path_start(op_at_start ? r_w_path : r_op_path),
+                                       path_end(op_at_end ? r_w_path : r_op_path)]),
+                fixed_l_op_path =
+                  open_polygonal_path([path_start(op_at_start ? l_w_path : l_op_path),
+                                       path_end(op_at_end ? l_w_path : l_op_path)]),
+                c_r_op_path = closed_path_for_height(translate(fixed_r_op_path, vz(op.loc.y)), op_height),
+                c_l_op_path = closed_path_for_height(translate(fixed_l_op_path, vz(op.loc.y)), op_height),
+                idxs = closest_vertices_indexes(path_vertices(c_r_w_path), path_vertices(c_r_op_path))
+              realize_pyramid_fustrum(b, w, "wall", c_r_op_path, c_l_op_path, false)
+              c_r_w_path =
+                closed_polygonal_path(
+                  inject_polygon_vertices_at_indexes(path_vertices(c_r_w_path), path_vertices(c_r_op_path), idxs))
+              c_l_w_path =
+                closed_polygonal_path(
+                  inject_polygon_vertices_at_indexes(path_vertices(c_l_w_path), path_vertices(c_l_op_path), idxs))
+              # preserve if not totally contained
+              ! (op.loc.x >= prevlength && op.loc.x + op.family.width <= currlength)
+            end
+          else
+            true
           end
         end
-        realize_pyramid_fustrum(b, w, "wall", path_vertices(r_w_path), path_vertices(l_w_path))
+        prevlength = currlength
+        realize_polygon(b, w, "wall", c_l_w_path, false)
+        realize_polygon(b, w, "wall", c_r_w_path, true)
       end
     end
     void_ref(b)
   end
+
+realize(b::Radiance, w::Window) = nothing
+realize(b::Radiance, w::Door) = nothing
 
 #=
 
