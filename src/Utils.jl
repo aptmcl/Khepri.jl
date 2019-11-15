4e9d66bafdb641fb04e275deb15d67c17ec7ed27
aptmcl <antonio.menezes.leitao@gmail.com>
2019-11-15 14:59:41 +0000

    Added function to generate error message.

--- src/Utils.jl
+++ src/Utils.jl
@@ -201,3 +201,10 @@ remap(in, (min_in, max_in), (min_out, max_out)) =
 
 cull(template, as) =
   [a for (a, t) in zip(as, cycle(template)) if t]
+
+
+## To present errors in the backends that call back to Julia
+export errormsg
+
+errormsg(e) =
+  sprint((io, e) -> showerror(io, e, backtrace(), backtrace=true), e)
