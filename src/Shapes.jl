a715855d7a96368a59ad1c09fa6f99ad4c58b4a7
aptmcl <antonio.menezes.leitao@gmail.com>
2019-11-23 12:15:48 +0000

    Added backward-compatible constructor to SocketBackend.

--- src/Shapes.jl
+++ src/Shapes.jl
@@ -1673,6 +1673,9 @@ struct SocketBackend{K,T} <: Backend{K,T}
   remote::NamedTuple
 end
 
+SocketBackend{K,T}(c::LazyParameter{TCPSocket}) where K,T =
+  SocketBackend{K,T}(c, ())
+
 connection(b::SocketBackend{K,T}) where {K,T} = b.connection()
 
 reset_backend(b::SocketBackend) =
