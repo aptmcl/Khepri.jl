5da3408980db76fd2c2afcf43fd555839bed311b
aptmcl <antonio.menezes.leitao@gmail.com>
2019-12-01 09:29:57 +0000

    BIM operations now create the corresponding layers in AutoCAD.

--- src/AutoCAD.jl
+++ src/AutoCAD.jl
@@ -293,18 +293,22 @@ end
 acad_layer_family(name, pairs...) = ACADLayerFamily(name, Parameter{Any}(nothing))
 backend_get_family_ref(b::ACAD, f::Family, af::ACADLayerFamily) = @remote(b, CreateLayer(af.name))
 
-set_backend_family(default_wall_family(), autocad, acad_layer_family("Walls"))
-set_backend_family(default_slab_family(), autocad, acad_layer_family("Slabs"))
-set_backend_family(default_roof_family(), autocad, acad_layer_family("Roofs"))
-set_backend_family(default_beam_family(), autocad, acad_layer_family("Beams"))
-set_backend_family(default_column_family(), autocad, acad_layer_family("Columns"))
-set_backend_family(default_door_family(), autocad, acad_layer_family("Doors"))
-set_backend_family(default_panel_family(), autocad, acad_layer_family("Panels"))
+set_backend_family(default_wall_family(), autocad, acad_layer_family("Wall"))
+set_backend_family(default_slab_family(), autocad, acad_layer_family("Slab"))
+set_backend_family(default_roof_family(), autocad, acad_layer_family("Roof"))
+set_backend_family(default_beam_family(), autocad, acad_layer_family("Beam"))
+set_backend_family(default_column_family(), autocad, acad_layer_family("Column"))
+set_backend_family(default_door_family(), autocad, acad_layer_family("Door"))
+set_backend_family(default_panel_family(), autocad, acad_layer_family("Panel"))
 
-set_backend_family(default_table_family(), autocad, acad_layer_family("Tables"))
-set_backend_family(default_chair_family(), autocad, acad_layer_family("Chairs"))
-set_backend_family(default_table_chair_family(), autocad, acad_layer_family("TablesChairs"))
+set_backend_family(default_table_family(), autocad, acad_layer_family("Table"))
+set_backend_family(default_chair_family(), autocad, acad_layer_family("Chair"))
+set_backend_family(default_table_chair_family(), autocad, acad_layer_family("TableChairs"))
 
+set_backend_family(default_curtain_wall_family().panel, autocad, acad_layer_family("CurtainWall-Panel"))
+set_backend_family(default_curtain_wall_family().boundary_frame, autocad, acad_layer_family("CurtainWall-Boundary"))
+set_backend_family(default_curtain_wall_family().transom_frame, autocad, acad_layer_family("CurtainWall-Transom"))
+set_backend_family(default_curtain_wall_family().mullion_frame, autocad, acad_layer_family("CurtainWall-Mullion"))
 #current_backend(autocad)
 
 
@@ -789,34 +793,41 @@ backend_rectangular_table_and_chairs(b::ACAD, c, angle, family) =
     @remote(b, TableAndChairs(c, angle, realize(b, family)))
 
 backend_slab(b::ACAD, profile, holes, thickness, family) =
-  let slab(profile) = map_ref(b, r -> @remote(b, Extrude(r, vz(thickness))),
-                              ensure_ref(b, backend_fill(b, profile))),
-      main_body = slab(profile),
-      holes_bodies = map(slab, holes)
-    foldl((r0, r1)->subtract_ref(b, r0, r1), holes_bodies, init=main_body)
+  with(current_layer, realize(b, family)) do
+    let slab(profile) = map_ref(b, r -> @remote(b, Extrude(r, vz(thickness))),
+                                ensure_ref(b, backend_fill(b, profile))),
+        main_body = slab(profile),
+        holes_bodies = map(slab, holes)
+      foldl((r0, r1)->subtract_ref(b, r0, r1), holes_bodies, init=main_body)
+    end
   end
 
 realize(b::ACAD, s::Beam) =
-  let profile = s.family.profile
-      profile_u0 = profile.corner
-      c = add_xy(s.cb, profile_u0.x + profile.dx/2, profile_u0.y + profile.dy/2)
-      # need to test whether it is rotation on center or on axis
-      o = loc_from_o_phi(s.cb, s.angle)
-    @remote(b, CenteredBox(add_y(o, -profile.dy/2), profile.dx, profile.dy, s.h))
+  with(current_layer, realize(b, s.family)) do
+    let profile = s.family.profile
+          profile_u0 = profile.corner
+          c = add_xy(s.cb, profile_u0.x + profile.dx/2, profile_u0.y + profile.dy/2)
+          # need to test whether it is rotation on center or on axis
+          o = loc_from_o_phi(s.cb, s.angle)
+        @remote(b, CenteredBox(add_y(o, -profile.dy/2), profile.dx, profile.dy, s.h))
+    end
   end
 #    @remote(b, CenteredBox(s.cb, vx(1, s.cb.cs), vy(1, s.cb.cs), s.family.width, s.family.height, s.h))
 
 #Columns are aligned along the center axis.
 realize(b::ACAD, s::FreeColumn) =
-  let profile = s.family.profile
-      profile_u0 = profile.corner
-      c = add_xy(s.cb, profile_u0.x + profile.dx/2, profile_u0.y + profile.dy/2)
-      # need to test whether it is rotation on center or on axis
-      o = loc_from_o_phi(c, s.angle)
-    @remote(b, CenteredBox(o, profile.dx, profile.dy, s.h))
+  with(current_layer, realize(b, s.family)) do
+    let profile = s.family.profile
+        profile_u0 = profile.corner
+        c = add_xy(s.cb, profile_u0.x + profile.dx/2, profile_u0.y + profile.dy/2)
+        # need to test whether it is rotation on center or on axis
+        o = loc_from_o_phi(c, s.angle)
+      @remote(b, CenteredBox(o, profile.dx, profile.dy, s.h))
+    end
   end
 
 realize(b::ACAD, s::Column) =
+  with(current_layer, realize(b, s.family)) do
     let profile = s.family.profile,
         profile_u0 = profile.corner,
         c = add_xy(s.cb, profile_u0.x + profile.dx/2, profile_u0.y + profile.dy/2),
@@ -826,6 +837,7 @@ realize(b::ACAD, s::Column) =
         o = loc_from_o_phi(s.cb + vz(base_height), s.angle)
       @remote(b, CenteredBox(add_y(o, -profile.dy/2), profile.dx, profile.dy, height))
     end
+  end
 
 backend_wall(b::ACAD, path, height, l_thickness, r_thickness, family) =
   #HACK: The thickness is wrong!!!!
@@ -842,7 +854,7 @@ set_view(camera::Loc, target::Loc, lens::Real, b::ACAD) =
   @remote(b, View(camera, target, lens))
 
 get_view(b::ACAD) =
-  @remote(b, ViewCamera(c)), @remote(b, ViewTarget(c)), @remote(b, ViewLens(c))
+  @remote(b, ViewCamera()), @remote(b, ViewTarget()), @remote(b, ViewLens())
 
 zoom_extents(b::ACAD) = @remote(b, ZoomExtents())
 
